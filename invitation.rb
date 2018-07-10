ed5f04f13dd04fd0a000ef29898f805e700fde47
nimishat <nimishathakkar83@gmail.com>
2018-07-10 14:06:00 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,7 +1,7 @@
-guest_name= gets.chomp.input
+guest_name= gets.chomp
 puts "Dear #{guest_name},"
 
-party_name=gets.chomp.input
+party_name=gets.chomp
 puts "You are cordially invited to the #{party_name} "
 
 
