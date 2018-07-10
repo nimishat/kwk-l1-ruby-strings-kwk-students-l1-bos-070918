cbaa4bb1cf778a87b84cc9cddd60d666183ac68a
nimishat <nimishathakkar83@gmail.com>
2018-07-10 14:12:37 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,8 +1,24 @@
+puts "Who would you like to write the invitation to."
 guest_name= gets.chomp
 puts "Dear #{guest_name},"
 
+puts "What is the name of the part?"
 party_name=gets.chomp
 puts "You are cordially invited to the #{party_name} "
+
+puts "What date will is the part on?"
+date = gets.chomp
+puts " on "
+
+puts "What time is the party at?"
+time=gets.chomp
+puts " at "
+puts "When do you want this person to RSVP by?"
+puts "Please RSVP no later than "
+
+puts "Who is this letter from?"
+host_name=gets.chomp
+puts "Sincerely, "
 
 
 
