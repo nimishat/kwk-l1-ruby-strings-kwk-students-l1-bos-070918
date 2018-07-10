40e5e805e7ac1f332914b1b1b77b2135839b7c0f
nimishat <nimishathakkar83@gmail.com>
2018-07-10 14:04:00 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,8 @@
-name= #{name}
-puts "Dear #{name},
-puts "You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30."
+guest_name= gets.chomp
+puts "Dear #{guest_name},"
+
+party_name=gets.chomp
+puts "You are cordially invited to the #{party_name} "
+
+
+
